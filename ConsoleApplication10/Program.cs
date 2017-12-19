903e1d481866255cad82567651cf865827331c9f
student <student@D40>
2017-12-19 19:33:33 +0200

    Interesting

--- ConsoleApplication10/Program.cs
+++ ConsoleApplication10/Program.cs
@@ -8,8 +8,54 @@ namespace ConsoleApplication10
 {
     class Program
     {
+        struct Viewer
+        {
+            public char Sex;
+            public byte Age;
+
+            //public Viewer(char Sex, byte Age)
+            //{
+            //    this.Sex = Sex;
+            //    this.Age = Age;
+            //}
+        }
         static void Main(string[] args)
         {
+            Viewer[,] viewers = new Viewer[8, 8];
+            Random rand = new Random();
+            int countOfPeople = rand.Next(18, 24);
+            char[] sex = { 'M', 'F' };
+            int x, y;
+
+
+            for (int i = 0; i < countOfPeople; i++)
+            {
+                while (true)
+                {
+                    x = rand.Next(0, 8);
+                    y = rand.Next(0, 8);
+
+                    if (viewers[x, y].Age == 0)
+                    {
+                        viewers[x, y] = new Viewer { Age = (byte)rand.Next(18, 60), Sex = sex[rand.Next(0, 2)] };
+                        break;
+                    }
+                    else continue;
+                }
+
+            }
+
+            for (int i = 0; i < viewers.GetLength(0); i++)
+            {
+                for (int j = 0; j < viewers.GetLength(1); j++)
+                {
+                    if (viewers[i, j].Age != 0)
+                        Console.Write($"  {viewers[i, j].Sex}  ");
+                    else
+                        Console.Write("  -  ");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
